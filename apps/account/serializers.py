f50c24a6c6fe3605532464eb4af258b8321ba6da
azamat <azamatovuser@gmail.com>
2023-11-22 20:52:06 +0500

    patch 0.0.7

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -14,7 +14,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Account
-        fields = ('username', 'password', 'password2', 'role')
+        fields = ('username', 'phone_number', 'email', 'password', 'password2', 'role')
 
     def validate(self, attrs):
         password = attrs.get('password')
