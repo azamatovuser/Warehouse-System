d641fc6913fe7fd67f698a9a14e25800903adae9
azamat <azamatovuser@gmail.com>
2023-12-02 12:10:33 +0500

    patch 1.0.2

--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -11,7 +11,15 @@ class DeviceCreateSerializer(serializers.ModelSerializer):
 class DeviceListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
-        fields = '__all__'
+        fields = ['id', 'client', 'name', 'imei']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        client = instance.client
+        representation['client'] = client.full_name if client and client.full_name else client.username if client and client.username else client.id if client else None
+
+        return representation
 
 
 class SpareCreateSerializer(serializers.ModelSerializer):
